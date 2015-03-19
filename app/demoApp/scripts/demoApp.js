36b77dbbed5d5b594d4b4bc3023afb4bb9e68500
Tilman Potthof <tilmanpotthof@googlemail.com>
2015-03-19 15:58:32 +0100

    improved demo with css config keys and added display area for the generated html (fixes #11)

--- app/demoApp/scripts/demoApp.js
+++ app/demoApp/scripts/demoApp.js
@@ -63,36 +63,47 @@ angular.module('paginationDemo').controller('demoController', function ($scope, 
     $scope.commits = commits;
     $scope.commentFilter = '';
   });
-}).controller('cssConfigController', function ($scope) {
+}).controller('cssConfigController', function ($scope, $compile) {
   $scope.cssConfigs = [
     {
-      label: 'Bootstrap 3.x',
+      label: 'Bootstrap 3.x (ul list)',
       path: 'bower_components/bootstrap-css-only/css/bootstrap.css',
       configKey: 'bootstrap3'
     },
     {
-      label: 'Bootstrap 2.x',
+      label: 'Bootstrap 2.x (div wrapped ul list)',
       path: 'demoApp/styles/bootstrap-2.3.2.css',
       configKey: 'bootstrap2'
     },
     {
-      label: 'List',
-      path: '',
+      label: 'ul list',
+      path: 'bower_components/bootstrap-css-only/css/bootstrap.css',
       configKey: 'list'
     },
     {
-      label: 'Div wrapped list',
-      path: '',
+      label: 'div wrapped ul list',
+      path: 'demoApp/styles/bootstrap-2.3.2.css',
       configKey: 'divWrappedList'
     }
   ];
 
   $scope.selectedCssConfig = $scope.cssConfigs[0];
 
+  function generateHtml() {
+    var template = '<div><st-pagination  collection="commits" css-config="CSS_CONFIG"></st-pagination></div>';
+    template = template.replace('CSS_CONFIG', $scope.selectedCssConfig.configKey);
+    return $compile(template)($scope).html().replace(/></g, '>\n<');
+  }
+
+  $scope.generatedHtml = generateHtml();
+
+
   $scope.$watch('selectedCssConfig', function (newConfig, oldConfig) {
     if (!angular.equals(newConfig, oldConfig)) {
       document.querySelector('link[href="' + oldConfig.path + '"]').remove();
       document.head.appendChild(angular.element('<link rel="stylesheet" href="' + newConfig.path + '" />')[0]);
+
+      $scope.generatedHtml = generateHtml();
     }
   });
 });
