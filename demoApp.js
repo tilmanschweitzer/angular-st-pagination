7a01d1ba4e5662beb5d85cfa75107600a6de50b8
Tilman Potthof <tilmanpotthof@googlemail.com>
2015-03-21 22:29:40 +0100

    updated to release 0.2.1

--- demoApp.js
+++ demoApp.js
@@ -1,80 +1,110 @@
 'use strict';
+
 angular.module('paginationDemo', [
   'ngRoute',
   'stPagination'
-]).config([
-  '$routeProvider',
-  function ($routeProvider) {
-    $routeProvider.when('/', { templateUrl: 'demoApp/views/demo.html' }).otherwise({ redirectTo: '/' });
+])
+  .config(["$routeProvider", function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'demoApp/views/demo.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
+
+angular.module('paginationDemo').controller('demoController', ["$scope", "$http", function ($scope, $http) {
+  function createCommit(line) {
+    var lineTokens = line.split(' ');
+    return {
+      hash: lineTokens[0],
+      comment: lineTokens.slice(1).join(' ')
+    };
   }
-]);
-angular.module('paginationDemo').controller('demoController', [
-  '$scope',
-  '$http',
-  function ($scope, $http) {
-    function createCommit(line) {
-      var lineTokens = line.split(' ');
-      return {
-        hash: lineTokens[0],
-        comment: lineTokens.slice(1).join(' ')
-      };
-    }
-    function filterEmptyLine(line) {
-      return !/(^\s*$)/.test(line);
-    }
-    $http.get('demoApp/data/angular-commits.txt').success(function (data) {
-      var lines = data.split('\n');
-      var commits = lines.filter(filterEmptyLine).map(createCommit);
-      $scope.functionNames = [
-        'limit',
-        'start',
-        'stop',
-        'page',
-        'displayPage',
-        'lastPage',
-        'totalPages',
-        'onFirstPage',
-        'onLastPage',
-        'length'
-      ];
-      $scope.displayProperties = [
-        'total',
-        'startIndex',
-        'stopIndex',
-        'currentPage',
-        'totalPages'
-      ];
-      $scope.propertyTemplate = function (property) {
-        return '{{ commits | stPageInfo:"' + property + '" }}';
-      };
-      $scope.getResult = function (functionName, object) {
-        return object[functionName]();
-      };
-      $scope.commits = commits;
-      $scope.commentFilter = '';
-    });
+
+  function filterEmptyLine(line) {
+    return !/(^\s*$)/.test(line);
   }
-]).controller('cssConfigController', [
-  '$scope',
-  function ($scope) {
-    $scope.cssConfigs = [
-      {
-        label: 'Bootstrap 3.x',
-        path: 'bower_components/bootstrap-css-only/css/bootstrap.css',
-        configKey: 'bootstrap3'
-      },
-      {
-        label: 'Bootstrap 2.x',
-        path: 'demoApp/styles/bootstrap-2.3.2.css',
-        configKey: 'bootstrap2'
-      }
+
+  $http.get('demoApp/data/angular-commits.txt').success(function (data) {
+    var lines = data.split('\n');
+    var commits = lines.filter(filterEmptyLine).map(createCommit);
+
+    $scope.functionNames = [
+      'limit',
+      'start',
+      'stop',
+      'page',
+      'displayPage',
+      'lastPage',
+      'totalPages',
+      'onFirstPage',
+      'onLastPage',
+      'length'
     ];
-    $scope.selectedCssConfig = $scope.cssConfigs[0];
-    $scope.$watch('selectedCssConfig', function (newConfig, oldConfig) {
-      if (!angular.equals(newConfig, oldConfig)) {
-        document.querySelector('link[href="' + oldConfig.path + '"]').remove();
-        document.head.appendChild(angular.element('<link rel="stylesheet" href="' + newConfig.path + '" />')[0]);
-      }
-    });
+
+    $scope.displayProperties = [
+      'total',
+      'startIndex',
+      'stopIndex',
+      'currentPage',
+      'totalPages'
+    ];
+
+    $scope.propertyTemplate = function (property) {
+      return '{{ commits | stPageInfo:"' + property + '" }}';
+    };
+
+    $scope.getResult = function (functionName, object) {
+      return object[functionName]();
+    };
+
+    $scope.commits = commits;
+    $scope.commentFilter = '';
+  });
+}]).controller('cssConfigController', ["$scope", "$compile", function ($scope, $compile) {
+  $scope.cssConfigs = [
+    {
+      label: 'Bootstrap 3.x (ul list)',
+      path: 'bower_components/bootstrap-css-only/css/bootstrap.css',
+      configKey: 'bootstrap3'
+    },
+    {
+      label: 'Bootstrap 2.x (div wrapped ul list)',
+      path: 'demoApp/styles/bootstrap-2.3.2.css',
+      configKey: 'bootstrap2'
+    },
+    {
+      label: 'ul list',
+      path: 'bower_components/bootstrap-css-only/css/bootstrap.css',
+      configKey: 'list'
+    },
+    {
+      label: 'div wrapped ul list',
+      path: 'demoApp/styles/bootstrap-2.3.2.css',
+      configKey: 'divWrappedList'
+    }
+  ];
+
+  $scope.selectedCssConfig = $scope.cssConfigs[0];
+
+  function generateHtml() {
+    var template = '<div><st-pagination  collection="commits" css-config="CSS_CONFIG"></st-pagination></div>';
+    template = template.replace('CSS_CONFIG', $scope.selectedCssConfig.configKey);
+    return $compile(template)($scope).html().replace(/></g, '>\n<');
   }
-]);
+
+  $scope.generatedHtml = generateHtml();
+
+
+  $scope.$watch('selectedCssConfig', function (newConfig, oldConfig) {
+    if (!angular.equals(newConfig, oldConfig)) {
+      document.querySelector('link[href="' + oldConfig.path + '"]').remove();
+      document.head.appendChild(angular.element('<link rel="stylesheet" href="' + newConfig.path + '" />')[0]);
+
+      $scope.generatedHtml = generateHtml();
+    }
+  });
+}]);
+
---
+++ docs/js/docs-setup.js
@@ -0,0 +1,63 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "stPagination",
+      "shortName": "stPagination",
+      "type": "overview",
+      "moduleName": "stPagination",
+      "shortDescription": "Provides client-side pagination through a simple angular filter.",
+      "keywords": "angular api client-side filter overview pagination simple stpagination"
+    },
+    {
+      "section": "api",
+      "id": "stPagination.directive:stPagination",
+      "shortName": "stPagination",
+      "type": "directive",
+      "moduleName": "stPagination",
+      "shortDescription": "Directive that display a pagination for a collection that is initialized by the",
+      "keywords": "$scope $watch alias aliases angular api app array attributes automatic basic bootstrap bootstrap2 bootstrap3 class click collection configure configured controller css css-config cssconfig current decide defined directive display displayed divwrappedlist edge-range edgerange element elements email er exampledata exampleusers filter fixed folded folding function html indices initialized input-small js jump key length list mid-range middle midrange module ng-controller ng-init ng-model ng-options ng-repeat overlap overlapped pagination paginationexample parameter range ranges replaced set setpage start startindex stopindex stpageinfo stpagination structure structures summed supported table total user usercontroller users wrapped"
+    },
+    {
+      "section": "api",
+      "id": "stPagination.directive:stPaginationLimit",
+      "shortName": "stPaginationLimit",
+      "type": "directive",
+      "moduleName": "stPagination",
+      "shortDescription": "Directive that displays a select element to change the number of items per page.",
+      "keywords": "$scope $watch angular api app array bootstrap2 change class collection configure configured controller css-config default directive displays element email exampledata exampleusers filter function html initialized input-small items js limit limits module ng-controller ng-repeat number options pagination paginationexample select setpage startindex stopindex stpageinfo stpagination table total user usercontroller users"
+    },
+    {
+      "section": "api",
+      "id": "stPagination.filter:stPageInfo",
+      "shortName": "stPageInfo",
+      "type": "filter",
+      "moduleName": "stPagination",
+      "shortDescription": "Returns information about pagination properties.",
+      "keywords": "$scope angular api app array badge bootstrap2 chained class collection comment comment-filter commentfilter comments commit commits controller css-config current currentpage element elements example examplecommits examplecontroller exampledata filter filters function hash html initialized js label-primary module ng-controller ng-model ng-repeat number pagination paginationexample properties property propertyname propertytemplate return returns startindex stopindex stpageinfo stpagination table text total totalpages type users"
+    },
+    {
+      "section": "api",
+      "id": "stPagination.filter:stPagination",
+      "shortName": "stPagination",
+      "type": "filter",
+      "moduleName": "stPagination",
+      "shortDescription": "Initialized the pagination and returns a new limited sub-array with an offset controlled by a",
+      "keywords": "$scope angular api app apply array attached bootstrap2 calculation chained class collection comment comment-filter commentfilter comments commit commitcontroller commits controlled controller correct correctly css-config defined directive email examplecommits exampledata exampleusers filter filtering filters function handles hash html initialized inputcollection js limit limited module ng-controller ng-model ng-repeat object offset offsets originalcollection paginated pagination paginationexample pass required returns source startindex stopindex stpageinfo stpagination sub-array table text total type usage user usercontroller usernamefilter users"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js",
+    "angular-st-pagination.js",
+    "exampleData.js"
+  ]
+};
