b335740e090c2f478d08f63b34ce351167931c7e
Tilman Potthof <tilmanpotthof@googlemail.com>
2015-04-08 02:20:01 +0200

    bower release v0.4.0

--- angular-st-pagination.js
+++ angular-st-pagination.js
@@ -1,55 +1,50 @@
 /*!
- * angular-st-pagination v0.3.0
+ * angular-st-pagination v0.4.0
  * source: git@github.com:tilmanpotthof/angular-st-pagination.git
  * license: MIT (https://raw.githubusercontent.com/tilmanpotthof/angular-st-pagination/master/LICENCE)
  */
-angular.module("stPagination", []);
-
-angular.module("stPagination").factory("StPagination", [ "indexUtil", function(indexUtil) {
+(function(angular) {
+  angular.module("stPagination", []);
   "use strict";
-  function StPagination(inputCollection) {
-    this._inputCollection = inputCollection;
-    this._limit = 10;
+  function Pagination(collection) {
+    this._collection = collection;
+    this._limit = Pagination.DEFAULT_LIMIT;
     this._page = 0;
-    this._cachedReducedIndices = {};
+    this._cachedIndices = {};
   }
-  function hasPagination(collection) {
-    return collection && collection.pagination instanceof StPagination;
-  }
-  function isNumberOrDefault(number, defaultValue) {
+  Pagination.isNumberOrDefault = function(number, defaultValue) {
     return angular.isNumber(number) ? number : defaultValue;
-  }
-  StPagination.hasPagination = hasPagination;
-  angular.extend(StPagination.prototype, {
-    setInputCollection: function(inputCollection) {
-      this._inputCollection = inputCollection;
+  };
+  Pagination.DEFAULT_LIMIT = 10;
+  Pagination.DEFAULT_EDGE_RANGE = 3;
+  Pagination.DEFAULT_MID_RANGE = 3;
+  angular.extend(Pagination.prototype, {
+    setCollection: function(collection) {
+      this._collection = collection;
       this.checkPageLimits();
     },
-    paginatedInputCollection: function() {
-      return this._inputCollection.slice(this.start(), this.stop());
+    paginatedCollection: function() {
+      return this._collection.slice(this.start(), this.stop());
     },
-    inputCollection: function() {
-      return this._inputCollection;
+    collection: function() {
+      return this._collection;
     },
     start: function() {
       return this.offset();
     },
     stop: function() {
+      var length = this.length();
       var stop = this.offset() + this.getLimit();
-      if (stop < this.length()) {
-        return stop;
-      } else {
-        return this.length();
-      }
+      return stop < length ? stop : length;
     },
     length: function() {
-      return this._inputCollection.length;
+      return this._collection.length;
     },
     setLimit: function(limit) {
       this._limit = limit;
     },
     totalPages: function() {
-      return Math.ceil(this._inputCollection.length / this.getLimit()) || 1;
+      return Math.ceil(this._collection.length / this.getLimit()) || 1;
     },
     offset: function() {
       return this._page * this._limit;
@@ -97,17 +92,16 @@ angular.module("stPagination").factory("StPagination", [ "indexUtil", function(i
       return this.totalPages() - 1;
     },
     reducedIndices: function(midRange, edgeRange) {
-      midRange = isNumberOrDefault(midRange, 3);
-      edgeRange = isNumberOrDefault(edgeRange, 3);
+      midRange = Pagination.isNumberOrDefault(midRange, Pagination.DEFAULT_MID_RANGE);
+      edgeRange = Pagination.isNumberOrDefault(edgeRange, Pagination.DEFAULT_EDGE_RANGE);
       var indexCacheKey = this.indexCacheKey(midRange, edgeRange);
-      if (this._cachedReducedIndices[indexCacheKey]) {
-        return this._cachedReducedIndices[indexCacheKey];
+      if (this._cachedIndices[indexCacheKey]) {
+        return this._cachedIndices[indexCacheKey];
       } else {
-        var page = this.page();
-        var total = this.totalPages();
-        var rangeBuilder = indexUtil.rangeBuilder(total).foldWithMidAndEdgeRangeForIndex(page, midRange, edgeRange);
+        var rangeBuilder = new RangeBuilder(this.totalPages());
+        rangeBuilder.foldForIndex(this.page(), midRange, edgeRange);
         var indices = rangeBuilder.build();
-        this._cachedReducedIndices[indexCacheKey] = indices;
+        this._cachedIndices[indexCacheKey] = indices;
         return indices;
       }
     },
@@ -121,47 +115,19 @@ angular.module("stPagination").factory("StPagination", [ "indexUtil", function(i
       return this.start() + 1;
     }
   });
-  return StPagination;
-} ]);
-
-angular.module("stPagination").filter("stPagination", [ "StPagination", function(StPagination) {
   "use strict";
-  return function stPaginationFilter(inputCollection, originalCollection) {
-    var collectionWithPaginationHandle;
-    if (!inputCollection) {
-      return;
-    }
-    collectionWithPaginationHandle = originalCollection || inputCollection;
-    if (!StPagination.hasPagination(collectionWithPaginationHandle)) {
-      collectionWithPaginationHandle.pagination = new StPagination(inputCollection);
-    }
-    var pagination = collectionWithPaginationHandle.pagination;
-    pagination.setInputCollection(inputCollection);
-    return pagination.paginatedInputCollection();
-  };
-} ]);
-
-angular.module("stPagination").factory("indexUtil", function() {
-  "use strict";
-  function RangeBuilder(array) {
-    this.array = array;
-    this.lastIndex = array.length - 1;
+  function RangeBuilder(rangeLength) {
+    this.array = RangeBuilder.range(rangeLength);
+    this.lastIndex = rangeLength - 1;
   }
+  RangeBuilder.range = function(length) {
+    return Array.apply(null, new Array(length)).map(function(_, i) {
+      return i;
+    });
+  };
   angular.extend(RangeBuilder.prototype, {
     build: function() {
       return this.array;
-    },
-    foldGreaterThan: function(offset) {
-      return this.foldRange(offset + 1, this.lastIndex);
-    },
-    foldGreaterEquals: function(offset) {
-      return this.foldRange(offset, this.lastIndex);
-    },
-    foldLessThan: function(limit) {
-      return this.foldRange(0, limit - 1);
-    },
-    foldLessEquals: function(limit) {
-      return this.foldRange(0, limit);
     },
     foldRange: function(start, stop) {
       var oldArray = this.array;
@@ -180,10 +146,7 @@ angular.module("stPagination").factory("indexUtil", function() {
       });
       return this;
     },
-    foldFixedLengthForIndex: function(index, length) {
-      return this.foldWithMidAndEdgeRangeForIndex(index, length, length);
-    },
-    foldWithMidAndEdgeRangeForIndex: function(index, midRange, edgeRange) {
+    foldForIndex: function(index, midRange, edgeRange) {
       var firstFoldStart = 0 + edgeRange;
       var firstFoldStop = index - midRange;
       var secondFoldStart = index + midRange;
@@ -199,161 +162,203 @@ angular.module("stPagination").factory("indexUtil", function() {
       }
     }
   });
-  function range(length) {
-    return Array.apply(null, new Array(length)).map(function(_, i) {
-      return i;
-    });
-  }
-  function rangeBuilder(length) {
-    return new RangeBuilder(range(length));
-  }
-  return {
-    range: range,
-    rangeBuilder: rangeBuilder
-  };
-});
-
-angular.module("stPagination").filter("displayPaginationIndex", function() {
-  "use strict";
-  return function(index) {
-    if (angular.isNumber(index)) {
-      return index + 1;
-    } else if (angular.isArray(index)) {
-      return "...";
-    } else {
-      return index;
-    }
-  };
-});
-
-angular.module("stPagination").directive("stPaginationLimit", [ "StPagination", function(StPagination) {
-  "use strict";
-  var DEFAULT_LIMITS = [ 10, 20, 50 ];
-  return {
-    restrict: "E",
-    replace: true,
-    template: '<select ng-options="limit for limit in limits()" ng-model="pagination._limit"></select>',
-    scope: {
-      collection: "=",
-      getLimits: "&limits"
-    },
-    link: function($scope) {
-      $scope.limits = function() {
-        return $scope.getLimits() || DEFAULT_LIMITS;
-      };
-      $scope.$watch("collection", function(collection) {
-        if (StPagination.hasPagination(collection)) {
-          $scope.pagination = collection.pagination;
-        } else {
-          delete $scope.pagination;
+  angular.module("stPagination").provider("stPagination", function() {
+    "use strict";
+    var DEFAULT_CSS_CONFG = {};
+    this.setDefaultLimit = function(defaultLimit) {
+      Pagination.DEFAULT_LIMIT = defaultLimit;
+    };
+    this.setDefaultEdgeRange = function(defaultEdgeRange) {
+      Pagination.DEFAULT_EDGE_RANGE = defaultEdgeRange;
+    };
+    this.setDefaultMidRange = function(defaultMidRange) {
+      Pagination.DEFAULT_MID_RANGE = defaultMidRange;
+    };
+    this.setDefaultCssConfig = function(cssConfig) {
+      DEFAULT_CSS_CONFG = cssConfig;
+    };
+    this.$get = function() {
+      return {
+        hasPagination: function(collection) {
+          return collection && collection.pagination instanceof Pagination;
+        },
+        Pagination: Pagination,
+        range: RangeBuilder.range,
+        indexRangeBuilder: function(length) {
+          return new RangeBuilder(length);
+        },
+        cssConfig: function() {
+          return DEFAULT_CSS_CONFG;
         }
-      });
-    }
-  };
-} ]);
-
-angular.module("stPagination").directive("stPagination", [ "StPagination", "$parse", function(StPagination, $parse) {
-  "use strict";
-  var css3UserSelectAliases = [ "-webkit-touch-callout", "-webkit-user-select", "-moz-user-select", "-ms-user-select", "user-select" ];
-  var basePagination = "<ul>" + '<li ng-class="{%DISABLED_CLASS%: pagination.onFirstPage()}">' + '<a ng-click="pagination.prev()">&laquo;</a>' + "</li>" + '<li ng-class="{%SELECTED_CLASS%: pagination.onPage(index)}" ' + 'ng-repeat="index in pagination.reducedIndices(midRange, edgeRange)">' + '<a ng-click="pagination.setPage(index)">{{ index | displayPaginationIndex }}</a>' + "</li>" + '<li ng-class="{%DISABLED_CLASS%: pagination.onLastPage()}">' + '<a ng-click="pagination.next()">&raquo;</a>' + "</li>" + "</ul>";
-  function extendDefaults(options) {
-    return angular.extend({
-      divWrapped: false,
-      selectedClass: "active",
-      disabledClass: "disabled"
-    }, options);
-  }
-  var cssConfigsByKey = {
-    list: {},
-    divWrappedList: {
-      divWrapped: true
-    },
-    bootstrap3: {},
-    bootstrap2: {
-      divWrapped: true
-    },
-    zurbFoundation: {
-      selectedClass: "current",
-      disabledClass: "unavailable"
-    }
-  };
-  var allowedValues = '"' + Object.keys(cssConfigsByKey).join('", "') + '"';
-  var defaultCssConfig = "list";
-  function parseCssConfig(cssConfig) {
-    var configObject = $parse(cssConfig)({});
-    if (angular.isObject(configObject)) {
-      return extendDefaults(configObject);
-    }
-    cssConfig = cssConfig || defaultCssConfig;
-    configObject = cssConfigsByKey[cssConfig];
-    if (configObject !== undefined) {
-      return extendDefaults(configObject);
-    } else {
-      var msg = 'Given css-config attribute "' + cssConfig + '" is not in allowed values ' + allowedValues;
-      throw new Error(msg);
-    }
-  }
-  return {
-    restrict: "E",
-    replace: true,
-    scope: {
-      collection: "=",
-      edgeRange: "=",
-      midRange: "="
-    },
-    template: basePagination,
-    compile: function($element, attributes) {
-      var cssConfigObject = parseCssConfig(attributes.cssConfig);
-      if (cssConfigObject.divWrapped) {
-        $element.wrap('<div class="pagination"></div>');
-      } else {
-        $element.addClass("pagination");
+      };
+    };
+  });
+  angular.module("stPagination").filter("stPagination", [ "stPagination", function(stPagination) {
+    "use strict";
+    return function stPaginationFilter(inputCollection, originalCollection) {
+      var collectionWithPaginationHandle;
+      if (!inputCollection) {
+        return;
       }
-      angular.forEach($element.find("li"), function(liElement) {
-        var $liElement = angular.element(liElement);
-        var ngClass = $liElement.attr("ng-class");
-        ngClass = ngClass.replace("%DISABLED_CLASS%", cssConfigObject.disabledClass);
-        ngClass = ngClass.replace("%SELECTED_CLASS%", cssConfigObject.selectedClass);
-        $liElement.attr("ng-class", ngClass);
-      });
-    },
-    controller: [ "$scope", "$element", "$attrs", function($scope, $element, $attrs) {
-      angular.forEach(css3UserSelectAliases, function(alias) {
-        $element.css(alias, "none");
-      });
-      var collectionName = $attrs.collection;
-      $scope.$watch("collection", function(collection) {
-        if (angular.isArray(collection)) {
-          if (StPagination.hasPagination(collection)) {
+      collectionWithPaginationHandle = originalCollection || inputCollection;
+      if (!stPagination.hasPagination(collectionWithPaginationHandle)) {
+        collectionWithPaginationHandle.pagination = new stPagination.Pagination(inputCollection);
+      }
+      var pagination = collectionWithPaginationHandle.pagination;
+      pagination.setCollection(inputCollection);
+      return pagination.paginatedCollection();
+    };
+  } ]);
+  angular.module("stPagination").directive("stPaginationLimit", [ "stPagination", function(stPagination) {
+    "use strict";
+    var DEFAULT_LIMITS = [ 10, 20, 50 ];
+    return {
+      restrict: "E",
+      replace: true,
+      template: '<select ng-options="limit for limit in limits()" ng-model="pagination._limit"></select>',
+      scope: {
+        collection: "=",
+        getLimits: "&limits"
+      },
+      link: function($scope) {
+        $scope.limits = function() {
+          return $scope.getLimits() || DEFAULT_LIMITS;
+        };
+        $scope.$watch("collection", function(collection) {
+          if (stPagination.hasPagination(collection)) {
             $scope.pagination = collection.pagination;
           } else {
-            var msg = 'Collection "' + collectionName + '" in the pagination directive is not used with a neccessary ' + "pagination filter.";
-            throw new Error(msg);
+            delete $scope.pagination;
           }
-        }
-      });
-    } ]
-  };
-} ]);
-
-angular.module("stPagination").filter("stPageInfo", [ "StPagination", function(StPagination) {
-  "use strict";
-  var propertyNameToFunctionMapping = {
-    total: "length",
-    totalPages: "totalPages",
-    currentPage: "displayPage",
-    startIndex: "displayStart",
-    stopIndex: "stop"
-  };
-  return function(collection, propertyName) {
-    if (StPagination.hasPagination(collection) && propertyName) {
-      var fnName = propertyNameToFunctionMapping[propertyName];
-      if (!fnName) {
-        throw new Error('No display property "' + propertyName + '" defined for the stPageInfo filter');
+        });
       }
-      return collection.pagination[fnName]();
-    } else {
-      return collection;
+    };
+  } ]);
+  angular.module("stPagination").directive("stPagination", [ "stPagination", "$parse", function(stPagination, $parse) {
+    "use strict";
+    var css3UserSelectAliases = [ "-webkit-touch-callout", "-webkit-user-select", "-moz-user-select", "-ms-user-select", "user-select" ];
+    var basePagination = "<ul>" + '<li ng-class="{%DISABLED_CLASS%: pagination.onFirstPage()}">' + '<a ng-click="pagination.prev()">&laquo;</a>' + "</li>" + '<li ng-class="{%SELECTED_CLASS%: pagination.onPage(index)}" ' + 'ng-repeat="index in pagination.reducedIndices(midRange, edgeRange)">' + '<a ng-click="pagination.setPage(index)">{{ displayPaginationIndex(index) }}</a>' + "</li>" + '<li ng-class="{%DISABLED_CLASS%: pagination.onLastPage()}">' + '<a ng-click="pagination.next()">&raquo;</a>' + "</li>" + "</ul>";
+    var cssConfigUtil = {
+      extendDefaults: function(options) {
+        var basicDefaults = {
+          divWrapped: false,
+          selectedClass: "active",
+          disabledClass: "disabled"
+        };
+        var defaults = stPagination.cssConfig();
+        if (!angular.isObject(defaults)) {
+          defaults = this.getCssConfigByKey(defaults);
+        }
+        var combinedDefaults = angular.extend(basicDefaults, defaults);
+        return angular.extend(combinedDefaults, options);
+      },
+      cssConfigsByKey: {
+        list: {},
+        divWrappedList: {
+          divWrapped: true
+        },
+        bootstrap3: {},
+        bootstrap2: {
+          divWrapped: true
+        },
+        zurbFoundation: {
+          selectedClass: "current",
+          disabledClass: "unavailable"
+        }
+      },
+      getCssConfigByKey: function(key) {
+        var configObject = this.cssConfigsByKey[key];
+        if (configObject !== undefined) {
+          return configObject;
+        } else {
+          var msg = 'Given css-config attribute "' + key + '" is not in allowed values ' + allowedValues;
+          throw new Error(msg);
+        }
+      },
+      parseCssConfig: function(cssConfig) {
+        var configObject = $parse(cssConfig)({});
+        if (angular.isObject(configObject)) {
+          return configObject;
+        }
+        cssConfig = cssConfig || defaultCssConfig;
+        configObject = this.getCssConfigByKey(cssConfig);
+        return configObject;
+      }
+    };
+    var allowedValues = '"' + Object.keys(cssConfigUtil.cssConfigsByKey).join('", "') + '"';
+    var defaultCssConfig = "list";
+    function displayPaginationIndex(index) {
+      if (angular.isNumber(index)) {
+        return index + 1;
+      } else if (angular.isArray(index)) {
+        return "...";
+      } else {
+        return index;
+      }
     }
-  };
-} ]);
+    return {
+      restrict: "E",
+      replace: true,
+      scope: {
+        collection: "=",
+        edgeRange: "=",
+        midRange: "="
+      },
+      template: basePagination,
+      compile: function($element, attributes) {
+        var cssConfigObject = cssConfigUtil.parseCssConfig(attributes.cssConfig);
+        cssConfigObject = cssConfigUtil.extendDefaults(cssConfigObject);
+        if (cssConfigObject.divWrapped) {
+          $element.wrap('<div class="pagination"></div>');
+        } else {
+          $element.addClass("pagination");
+        }
+        angular.forEach($element.find("li"), function(liElement) {
+          var $liElement = angular.element(liElement);
+          var ngClass = $liElement.attr("ng-class");
+          ngClass = ngClass.replace("%DISABLED_CLASS%", cssConfigObject.disabledClass);
+          ngClass = ngClass.replace("%SELECTED_CLASS%", cssConfigObject.selectedClass);
+          $liElement.attr("ng-class", ngClass);
+        });
+      },
+      controller: [ "$scope", "$element", "$attrs", function($scope, $element, $attrs) {
+        angular.forEach(css3UserSelectAliases, function(alias) {
+          $element.css(alias, "none");
+        });
+        var collectionName = $attrs.collection;
+        $scope.displayPaginationIndex = displayPaginationIndex;
+        $scope.$watch("collection", function(collection) {
+          if (angular.isArray(collection)) {
+            if (stPagination.hasPagination(collection)) {
+              $scope.pagination = collection.pagination;
+            } else {
+              var msg = 'Collection "' + collectionName + '" in the pagination directive is not used with a neccessary ' + "pagination filter.";
+              throw new Error(msg);
+            }
+          }
+        });
+      } ]
+    };
+  } ]);
+  angular.module("stPagination").filter("stPageInfo", [ "stPagination", function(stPagination) {
+    "use strict";
+    var propertyNameToFunctionMapping = {
+      total: "length",
+      totalPages: "totalPages",
+      currentPage: "displayPage",
+      startIndex: "displayStart",
+      stopIndex: "stop"
+    };
+    return function(collection, propertyName) {
+      if (stPagination.hasPagination(collection) && propertyName) {
+        var fnName = propertyNameToFunctionMapping[propertyName];
+        if (!fnName) {
+          throw new Error('No display property "' + propertyName + '" defined for the stPageInfo filter');
+        }
+        return collection.pagination[fnName]();
+      } else {
+        return collection;
+      }
+    };
+  } ]);
+})(angular);
